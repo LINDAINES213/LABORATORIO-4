273a77dce8aa48f981748fb46722c240032b8094
LINDA JIMÉNEZ <lijv1333@gmail.com>
2021-11-16 21:57:21 -0600

    Creacion MAIN

--- Main.java
+++ Main.java
@@ -1,7 +1,36 @@
 public class Main {
 
     public static void main(String[] args){
-        
+
+       RadioA radioA = new RadioA();
+       RadioS radioS = new RadioS();
+       RadioC radioC = new RadioC();
+       Vista vista = new Vista();
+
+       int opcion1 = 0;
+
+       while (opcion1 != 4){
+
+           opcion1 = vista.menuPrincipal();
+
+           if (opcion1 == 1){
+
+                radioS.Radios(opcion1, opcion1, opcion1, opcion1);
+
+           } else if (opcion1 == 2){
+
+                radioA.Radios(opcion1, opcion1, opcion1, opcion1);
+
+           } else if (opcion1 == 3){
+
+                radioC.Radios(opcion1, opcion1, opcion1, opcion1);
+
+           } else if (opcion1 == 4){
+
+               vista.Despedida();
+
+           }
+       }
     }
     
 }
