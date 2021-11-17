51c83e7668fc0a8a291ee19abf270e9608d970dd
LINDA JIMÉNEZ <lijv1333@gmail.com>
2021-11-16 21:57:35 -0600

    Creacion de Vista

--- Vista.java
+++ Vista.java
@@ -1,3 +1,33 @@
+import java.util.Scanner;
+
 public class Vista {
+
+    Scanner sn = new Scanner(System.in);
     
+    public int menuPrincipal() {
+
+        int opcion1;
+
+        System.out.println();
+        System.out.println();
+        System.out.println("------------ Mercedes-Benz ------------");
+        System.out.println();
+        System.out.println("Bienvendo a Mercedes-Benz!");
+        System.out.println("Puedes probar cualquier radio dependiendo el modelo del carro que elijas");
+        System.out.println("Hora de Empezar! Que radio deseas probar?");
+        System.out.println("\n---------------------------------------------------------------------------------------------------------------------");
+
+        String menuPrincipal = "1. Radio para Carro Modelo S\n" + 
+                                "2. Radio para Carro Modelo A\n" + 
+                                "3. Radio para Carro Modelo C\n" + 
+                                "4. Salir del programa\n";
+
+        System.out.println(menuPrincipal);
+        opcion1 = sn.nextInt();
+        return opcion1;
+    }
+
+    public void Despedida() {
+        System.out.println("\nMuchas gracias por utilizar la aplicación! Vuelva pronto!\n");
+    }
 }
